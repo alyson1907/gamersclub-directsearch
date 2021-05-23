299412d61c45559c85b0523c4cc10e0765bbef83
Alyson Matheus <alyson1907@gmail.com>
2021-05-23 19:56:08 -0300

    Redirecting in background page whenever Steam profile is open

---
+++ directSearch.js
@@ -0,0 +1,8 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!changeInfo.status?.toLowerCase().includes('loading')) return
+  const matches = [/^http(s)?:\/\/steamcommunity.com\/id\//g, /^http(s)?:\/\/steamcommunity.com\/profiles\//g]
+  const shouldRedirect = matches.some(pattern => pattern.test(tab.url))
+  if (shouldRedirect) {
+    chrome.tabs.update(tabId, { url: `https://gamersclub.com.br/buscar?busca=${tab.url}` })
+  }
+})
