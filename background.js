c515e37530fd2e8e0d02d53cf09701722f7bbb8e
Alyson Matheus <alyson1907@gmail.com>
2021-05-24 14:33:28 -0300

    Adds condition to only redirect when extension is activated

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
+// Listen to open tabs URLs and redirects to GamersClub search
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (!changeInfo.status?.toLowerCase().includes('loading')) return
   const matches = [/^http(s)?:\/\/steamcommunity.com\/id\//g, /^http(s)?:\/\/steamcommunity.com\/profiles\//g]
   const shouldRedirect = matches.some(pattern => pattern.test(tab.url))
-  shouldRedirect && chrome.tabs.update(tabId, { url: `https://gamersclub.com.br/buscar?busca=${tab.url}` })
+  getStorage(activatedKey, (state) => {
+    const isActivated = state[activatedKey]
+    if (shouldRedirect && isActivated) chrome.tabs.update(tabId, { url: `https://gamersclub.com.br/buscar?busca=${tab.url}` })
+  })
 })
